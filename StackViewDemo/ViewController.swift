19ad1815aa9dae6721ad44c52fc50d5447098753
Viswanath Subramani S S <viswanathsubramaniss@gmail.com>
2017-12-09 22:17:01 +0530

    Demo Usage

---
+++ StackViewDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StackViewDemo
+//
+//  Created by Viswanath Subramani S S
+//	Assets provided by AppCoda
+//  Copyright © 2017 ViswanathSubramaniSS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
